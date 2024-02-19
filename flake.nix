9a773fea7347f6c38d9082fcbc760b4c485ec016
山下 <git@yamashit.ax>
2024-02-19 20:05:16 +0900

    Update to nixpkgs unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Zig compiler binaries.";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
 
     # Used for shell.nix
